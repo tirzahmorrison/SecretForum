8cfe594b695fb66f7d7761d21d54b0be9def003c
Young <wyoung@proforma.com>
2018-04-30 17:13:16 -0400

    Inserted Categories

--- SecretForum/Scripts/app/app.js
+++ SecretForum/Scripts/app/app.js
@@ -16,16 +16,25 @@ app.config(function ($routeProvider) {
 });
 
 app.controller('dashboardController' , ['$scope', '$http', function ($scope, $http) {
+    console.log('dashboardController');
 
     const init = () => {
+        console.log('dashboardController > init');
+
         $scope.categories = [];
 
         $http({
             method: 'GET',
             url: '/api/categories'
         })
-            .then(response => response.data)
-            .then(data => $scope.categories = data);
+            .then(response => {
+                console.log('Response for categories recieved');
+                return response.data;
+            })
+            .then(data => {
+                console.log(data);
+                $scope.categories = data;
+            });
     };
 
     init();
